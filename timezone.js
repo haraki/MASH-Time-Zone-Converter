c3efd3330e464113792b51fe6b3b1665c2476bc1
Masashi Haraki <masa.haraki@gmail.com>
2013-05-07 07:13:51 +0900

    国と都市を別々のセレクトメニューで設定するよう修正（未完成）
    Signed-off-by: Masashi Haraki <masa.haraki@gmail.com>

--- timezone.js
+++ timezone.js
@@ -1,28 +1,56 @@
 ﻿var timezoneData;
 
-function makeTimeZoneOption(tzData)
+function initializeOption(tzData)
 {
 	timezoneData = tzData;
 	
-	var src = $("#src_zone");
-	var dst = $("#dst_zone");
+	var src = $("#src_country");
+	var dst = $("#dst_country");
 	
-	for(var i = 0;i < tzData.length;i++)
+	var oldCountry = "";
+	
+	for(var i = 0;i < timezoneData.length;i++)
 	{
-		var option_str = "<option value = \"" + tzData[i].tz + "\">" + tzData[i].city + "," + tzData[i].country + "</option>";
+		if(timezoneData[i].country != oldCountry)
+		{
+			var option_str = "<option value = \"" + timezoneData[i].country + "\">" + timezoneData[i].country + "</option>";
 		
-		src.append(option_str);
-		dst.append(option_str);
+			src.append(option_str);
+			dst.append(option_str);
+			
+			oldCountry = timezoneData[i].country;
+		}
 	}
 	
-	src.selectmenu('refresh');
-	dst.selectmenu('refresh');
+	src.selectmenu('refresh', true);
+	dst.selectmenu('refresh', true);
+}
+
+function resetCitySelect(country, city_id)
+{
+	var len = $(city_id).length;
+	for(var i = 1;i < len;i++)
+	{
+		$(city_id).options[i] = null;
+	}
+	
+	for(var i = 0;i < timezoneData.length;i++)
+	{
+		if(timezoneData[i].country == country)
+		{
+			var option_str = "<option value = \"" + timezoneData[i].tz + "\">" + timezoneData[i].city + "</option>";
+			
+			$(city_id).append(option_str);
+		}
+	}
+	
+	$(city_id).selectmenu('refresh', true);
 }
 
 function translateTimeZone()
 {
-	var srcZone    = $("#src_zone").val();
-	var dstZone    = $("#dst_zone").val();
+	var srcZone    = $("#src_city").val();
+	var dstZone    = $("#dst_city").val();
 	var srcDateVal = $("#src_date").val();
 	var srcTimeVal = $("#src_time").val();
 	
@@ -50,12 +78,64 @@ function translateTimeZone()
 	$("#dst_time_text").text($("#dst_time").val());
 }
 
+function change_srcCountry()
+{
+	var srcCountry = $("#src_country").val();
+	
+	if(srcCountry == "")
+	{
+		$("#src_city").selectmenu('disable');
+		
+		return;
+	}
+	
+	resetCitySelect(srcCountry, '#src_city');
+	
+	$("#src_city").selectmenu('enable');
+}
+
+function change_dstCountry()
+{
+	var dstCountry = $("#dst_country").val();
+	
+	if(dstCountry == "")
+	{
+		$("#dst_city").selectmenu('disable');
+		
+		return;
+	}
+	
+	resetCitySelect(dstCountry, '#dst_city');
+	
+	$("#dst_city").selectmenu('enable');
+}
+
+function change_srcCity()
+{
+	translateTimeZone();
+}
+
+function change_dstCity()
+{
+	translateTimeZone();
+}
+
+function change_srcDate()
+{
+	translateTimeZone();
+}
+
+function change_srcTime()
+{
+	translateTimeZone();
+}
+
 function initialize()
 {
 	timezoneJS.timezone.zoneFileBasePath = './tz';
 	timezoneJS.timezone.init();
 	
-	$.getJSON('./timezone.json', makeTimeZoneOption);
+	$.getJSON('./timezone.json', initializeOption);
 }
 
 $(document).ready(initialize);
